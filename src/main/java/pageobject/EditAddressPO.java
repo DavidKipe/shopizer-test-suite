143d8650096b98da686e7f75067a518733861294
Davide <david91kipe@gmail.com>
2021-03-31 13:42:55 +0200

    Assertion checks for ItemReviewTests and AccountManagementTests. Finish of assertion checks.

--- src/main/java/pageobject/EditAddressPO.java
+++ src/main/java/pageobject/EditAddressPO.java
@@ -5,11 +5,33 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 
 public class EditAddressPO extends FooterNavigationPO {
 
+	@FindBy(how = How.XPATH, xpath = "//input[@id='firstName']")
+	WebElement firstNameInputElem;
+
+	@FindBy(how = How.XPATH, xpath = "//input[@id='lastName']")
+	WebElement lastNameInputElem;
+
 	@FindBy(how = How.XPATH, xpath = "//input[@id='address']")
 	WebElement streetAddressInputElem;
+
+	@FindBy(how = How.XPATH, xpath = "//input[@id='city']")
+	WebElement cityInputElem;
+
+	@FindBy(how = How.XPATH, xpath = "//select[@id='customer_country']")
+	WebElement countryInputElem;
+
+	@FindBy(how = How.XPATH, xpath = "//input[@id='hidden_zones']")
+	WebElement stateProvInputElem;
+
+	@FindBy(how = How.XPATH, xpath = "//input[@id='billingPostalCode']")
+	WebElement postalCodeInputElem;
+
+	@FindBy(how = How.XPATH, xpath = "//input[@id='phone']")
+	WebElement phoneNumberInputElem;
 
 	@FindBy(how = How.XPATH, xpath = "//input[@id='submitAddress']")
 	WebElement changeAddressBtnElem;
@@ -35,6 +57,39 @@ public class EditAddressPO extends FooterNavigationPO {
 
 		billingShippingInfoBtnElem.click();
 		return new BillingAndShippingAddressesInfoPO(driver);
+	}
+
+	public String getFirstName() {
+		return firstNameInputElem.getAttribute("value");
+	}
+
+	public String getLastName() {
+		return lastNameInputElem.getAttribute("value");
+	}
+
+	public String getAddress() {
+		return streetAddressInputElem.getAttribute("value");
+	}
+
+	public String getCity() {
+		return cityInputElem.getAttribute("value");
+	}
+
+	public String getCountry() {
+		Select countrySelect = new Select(countryInputElem);
+		return countrySelect.getFirstSelectedOption().getText();
+	}
+
+	public String getStateProv() {
+		return stateProvInputElem.getAttribute("value");
+	}
+
+	public String getPostalCode() {
+		return postalCodeInputElem.getAttribute("value");
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumberInputElem.getAttribute("value");
 	}
 
 }
